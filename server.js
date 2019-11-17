b6ab6d1aab0e6690ff72406ee6aa4ae99a0bc6e9
Leon-Agar <54464146+Leon-Agar@users.noreply.github.com>
2019-11-16 22:40:07 -0300

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ if (config.server.update) {
 
         if (config.server.version < requesterConfig.server.version) {
             logger.warn(`[SERVER] A new update was found!`)
-            logger.warn(`[SERVER] Download -> https://github.com/Leon-Agar/LegendBots`)
+            logger.warn(`[SERVER] Download -> https://jimboy3100.github.io/ExampleScripts/agario-bots2`)
         } else {
             logger.good(`[SERVER] No updates found!`)
         }
